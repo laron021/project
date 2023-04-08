6c69b9d1521e6baf4b58e8e2b7339e822c813ea5
laron021 <79050753+laron021@users.noreply.github.com>
2023-04-08 18:13:21 +0200

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -1,8 +1,9 @@
-# Check if the current user is root
+# import modules
 import os
 import sys
 import subprocess
 
+#DECLARE COLORS
 PURPLE = '\033[1;95m'
 RED = '\033[1;31m'
 BLUE = '\033[1;90m'
@@ -18,20 +19,53 @@ else:
     sys.exit(1)
 
 #CHECK IF THE NECCESARRY PROGRAMS ARE INSTLLED
-# Define the command to check if gobuster is installed
 command33 = "which nmap"
 # Run the command and capture the output
 result = subprocess.run(command33, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 # Check if the command was successful (exit code 0)
 if result.returncode == 0:
-    print("nmap is installed!")
+    pass
 else:
     print("nmap is not installed. Install it with sudo apt install nmap")
 
+#OWASP NETTACKER
+def install_owasp_nettacker():
+    git_clone_command = ["git", "clone", "https://github.com/OWASP/Nettacker.git"]
+    nettacker_directory = "Nettacker"
+
+    try:
+        print("Cloning OWASP Nettacker from GitHub...")
+        subprocess.check_output(git_clone_command)
+        print("Cloning completed.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error while cloning OWASP Nettacker: {e}")
+        sys.exit(1)
+
+    try:
+        print("Installing OWASP Nettacker dependencies...")
+        #os.chdir(nettacker_directory)
+        subprocess.check_output(["pip3", "install", "-r", "Nettacker/requirements.txt"])
+        print("Installation completed.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error while installing OWASP Nettacker dependencies: {e}")
+        sys.exit(1)
 
 
+def run_owasp_nettacker(target_ip):
+    nettacker_script = "nettacker.py"
+    command = f"python3 Nettacker/nettacker.py -i {target_ip} -m port_scan"
 
+    try:
+        print(f"Starting OWASP Nettacker on target IP: {target_ip}")
+        output = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # output = subprocess.check_output(["python3 " nettacker_script + " -i " + target_ip + " -M" + " dir_scan"])
+        print("OWASP Nettacker scan completed.")
+        print(output.stdout.decode("utf-8"))
+    except subprocess.CalledProcessError as e:
+        print(f"Error while running OWASP Nettacker: {e}")
+        sys.exit(1)
 
+#---------------------------------------------------------------------------
 print(BLUE + ''' 
 
 ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗
@@ -42,7 +76,7 @@ print(BLUE + '''
  ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝ 
          _______________________________________ 
         |  ___________________________________  |
-        | | basic information gathering toool | |
+        | | basic information gathering tool | |
         | |___________________________________| |
         |_______________________________________|
 ''' + ENDCOLOR)
@@ -73,13 +107,13 @@ def run_and_save(command, output_file):
 # Display the menu to the user
 def menu():
     #          bold, color
-    print(BLUE + '1: Scan with nmap' + ENDCOLOR)  # dark cyan text
-    print(BLUE+ '2: DNSRecon' + ENDCOLOR)  # dark cyan text
-    print(BLUE + '3: IP a' + ENDCOLOR)  # dark cyan text
-    print(BLUE + '4: Option four' + ENDCOLOR)  # dark cyan text
-    print(BLUE + '5: Option five' + ENDCOLOR)  # dark cyan text
-    print(BLUE + '6: Option six' + ENDCOLOR)  # dark cyan text
-    print('\033[1;32m' + '7: Exit program' + ENDCOLOR)  # dark cyan text
+    print(BLUE + '1: Scan with nmap' + ENDCOLOR)
+    print(BLUE+ '2: DNSRecon' + ENDCOLOR)
+    print(BLUE + '3: IP a' + ENDCOLOR)
+    print(BLUE + '4: install requirements' + ENDCOLOR)
+    print(BLUE + '5: OWASP Nettacker' + ENDCOLOR)
+    print(BLUE + '6: Option six' + ENDCOLOR)
+    print('\033[1;32m' + '7: Exit program' + ENDCOLOR)
 
 
 def option1():
@@ -106,10 +140,24 @@ def option3():
 def option4():
     print("Option 4 selected")
     # Add code to execute option 4 here
+    command = "pip install -r requirements.txt"
+    run_and_save(command, "output11.txt")
+    run_and_save("import requests", "output11.txt")
 
 def option5():
     print("Option 5 selected")
+    IPadress = input("site name: ")
     # Add code to execute option 5 here
+    isExist = os.path.exists("Nettacker")
+
+    if isExist == False:
+        install_owasp_nettacker()
+        run_owasp_nettacker(IPadress)
+
+    else:
+        print("OWASP Nettacker installed")
+        run_owasp_nettacker(IPadress)
+
 
 def option6():
     print("Option 6 selected")
